a503889beef9461afcccad279b4a7b32a8f05571
justinhuang917 <justinhuang917@gmail.com>
2012-12-19 20:01:09 +0800

    update command args

--- goftool/src/gof.go
+++ goftool/src/gof.go
@@ -4,37 +4,38 @@ import (
 	"flag"
 	"fmt"
 	"goftool"
+	"strings"
+)
+
+var (
+	action      = flag.String("action", "compileview", "compileview")
+	viewpath    = flag.String("viewpath", "./view", "view path")
+	outviewpath = flag.String("outviewpath", "./view", "out view path")
+	others      = flag.String("other", "", "for other args")
 )
 
 func main() {
 	flag.Parse()
-	var arg0 string
-	if flag.NArg() > 0 {
-		arg0 = flag.Arg(0)
-	}
+	// fmt.Println(*action)
+	// var arg0 string
+	// if flag.NArg() > 0 {
+	// 	arg0 = flag.Arg(0)
+	// }
 	switch {
-
-	case arg0 == "compileview": //Compile View
-		viewDir := "./view" //Default view path
-		if flag.NArg() > 1 {
-			viewDir = flag.Arg(1) //"./view/html/"
+	case *action == "compileview":
+		otherArgs := make([]string, 0, 0)
+		fmt.Println(*others)
+		if *others != "" {
+			otherArgs = strings.Split(*others, ",")
 		}
-		outputdir := viewDir
-		if flag.NArg() > 2 {
-			outputdir = flag.Arg(2)
-		}
-		err := goftool.Compile(viewDir, outputdir, flag.Args()[3:]...)
+		fmt.Println(otherArgs[0])
+		err := goftool.Compile(*viewpath, *outviewpath, otherArgs...)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-
-	case arg0 == "clearview":
-		viewDir := "./view" //Default view path
-		if flag.NArg() > 1 {
-			viewDir = flag.Arg(1) //"./view/html/"
-		}
-		err := goftool.Clear(viewDir)
+	case *action == "clearview":
+		err := goftool.Clear(*viewpath)
 		if err != nil {
 			fmt.Println(err)
 		}
